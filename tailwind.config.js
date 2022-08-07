d712f549f9280f905da436001376cc88b70daa96
Iddriss Raaj <idd.iraaj@gmail.com>
2022-08-07 16:00:03 +0000

    add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        green_light: "#00DA63",
+        green_light1: "#25AE63",
+        green_1: "#131E18",
+        green_2: "#162B20",
+        green_dark1: "#011809",
+        green_dark2: "#010E05",
+      },
       screens: {
         "3xl": "2400px",
       },
@@ -14,3 +21,6 @@ module.exports = {
   },
   plugins: [],
 };
+
+// linear-gradient(180deg, #06C05A 0%, #07652D 100%);
+// border: 1px solid rgba(0, 218, 99, 0.16)
