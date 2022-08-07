8c0022556b3d2cb9c43029b18d2d63da601b09df
Iddriss Raaj <idd.iraaj@gmail.com>
2022-08-07 20:06:47 +0000

    brief sections

--- components/sections/hero-brief/index.js
+++ components/sections/hero-brief/index.js
@@ -2,20 +2,21 @@ import styles from "./hero.module.css";
 import Image from "next/image";
 import Rocket from "../../../assets/icons/rocket.svg";
 import bgImage from "../../../assets/images/bg-video.jpeg";
+import ScrollArrow from "../../../assets/icons/scroll-arrow.svg";
 
 export function HeroBrief() {
   return (
     <section className={`${styles.container} max-container`}>
       <div className={styles.hero}>
         <div className={styles.hero_text}>
-          <h1 className="xl:text-[64px] lg:text-[50px] md:text-[35px] text-[30px] leading-[138%] text-white">
+          <h1 className="xl:text-[62px] lg:text-[50px] md:text-[35px] text-[30px] leading-[138%] text-white">
             Bringing
             <br className="md:hidden" />{" "}
             <span className=" text-green_light">StarkNet</span>{" "}
             <br className="md:hidden" />
             Closer to Your Doorstep.
           </h1>
-          <p className="font-CeraPro-Light xl:text-[20px] md:text-base text-sm leading-[160%] text-white pt-4">
+          <p className="font-CeraPro-Light xl:text-[20px] md:text-[16px] text-sm leading-[160%] text-white pt-4">
             Earn, Swap and Moon on the most used Stark Platform ever!
             <span className="inline-block pl-2">
               <Rocket />
@@ -48,7 +49,26 @@ export function HeroBrief() {
           />
         </div>
       </div>
-      <div className={styles.brief}></div>
+      <div className={styles.brief}>
+        <h2 className="xl:text-[36.29px] lg:text-[30.29px] md:text-[22.29px] text-[18px] leading-[160%] text-white text-center">
+          …Your one-stop hub for Defi Solutions on Starknet
+        </h2>
+        <h2 className="xl:text-[36.29px]  lg:text-[30.29px] md:text-[22.29px] text-[18px] leading-[160%] text-green_light text-center">
+          Don’t be left out, a change is coming…
+        </h2>
+        <p className=" font-CeraPro-Light text-white opacity-[0.65] xl:text-[20px] lg:text-[18px] md:text-[16px] text-[14px] text-center pt-8 lg:pb-12 pb-4">
+          StarkDeFi is a hub for defi solutions on StarkNet, looking to improve
+          the adoption of blockchain technology. StarkNet provides unlimited
+          possibilities for innovative solutions (Scalability) on the
+          blockchain. Our aim is to facilitate and simplify the onboarding
+          process for newbies in the blockchain space with a hub that
+          accommodates a variety of deFi solutions. For us, DeFi is not just for
+          the veterans in the space, but for the ordinary person as well.
+        </p>
+        <button>
+          <ScrollArrow className="lg:scale-100 md:scale-90 scale-50" />
+        </button>
+      </div>
     </section>
   );
 }
