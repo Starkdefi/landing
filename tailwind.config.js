df173c766bb2acf666bccea92d34808e23b7d90c
Iddriss Raaj <idd.iraaj@gmail.com>
2022-08-07 15:06:47 +0000

    init tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {},
+      screens: {
+        "3xl": "2400px",
+      },
+    },
+  },
+  plugins: [],
+};
