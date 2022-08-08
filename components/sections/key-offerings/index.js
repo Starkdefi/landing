56c292fa4858bf2b52df21f8c9f772d28f2a7ee6
Iddriss Raaj <idd.iraaj@gmail.com>
2022-08-08 20:22:24 +0000

    fade in keyoffering heading

--- components/sections/key-offerings/index.js
+++ components/sections/key-offerings/index.js
@@ -7,6 +7,7 @@ import mintIllustration from "../../../assets/images/mint-asset.png";
 import swapIllustration from "../../../assets/images/swap-asset.png";
 import stakeIllustration from "../../../assets/images/stake-assets.png";
 import FeatureCard from "../../feature-card";
+import { Fade } from "react-awesome-reveal";
 
 const offers = [
   {
@@ -39,19 +40,21 @@ export function KeyOffering() {
   return (
     <section className={`${styles.container} max-container `}>
       <div className="flex flex-col items-center justify-center xl:pt-28 lg:pt-20 pt-14">
-        <Title className="text-white text-center">
-          Our Key <span className="text-green_light">Offerings</span>
-        </Title>
-        <Text className="text-white opacity-[0.65] pt-4 text-center max-w-[54rem]">
-          As your go-to defi hub, we are offering a wide range of defi solutions
-          tailored at taking full advantage of the scalable and economical
-          features of StarkNet.
-        </Text>
-        <SimpleLink
-          className="text-white opacity-[0.85] text-[16px] pt-4 hover:text-green_light"
-          text="See all products"
-          url="/#"
-        />
+        <Fade cascade="true" direction="up" triggerOnce="true">
+          <Title className="text-white text-center">
+            Our Key <span className="text-green_light">Offerings</span>
+          </Title>
+          <Text className="text-white opacity-[0.65] pt-4 text-center max-w-[54rem]">
+            As your go-to defi hub, we are offering a wide range of defi
+            solutions tailored at taking full advantage of the scalable and
+            economical features of StarkNet.
+          </Text>
+          <SimpleLink
+            className="text-white opacity-[0.85] text-[16px] pt-4 hover:text-green_light"
+            text="See all products"
+            url="/#"
+          />
+        </Fade>
       </div>
       <div className="flex items-center lg:justify-between justify-center pt-20 md:flex-nowrap flex-wrap">
         {offers.map((offer, i) => (
