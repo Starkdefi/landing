0787848ea783b445486ed4b3f0be9ded9e8b3a1e
Iddriss Raaj <idd.iraaj@gmail.com>
2022-08-10 10:05:27 +0000

    add gtag

---
+++ pages/_document.js
@@ -0,0 +1,32 @@
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+        <Script
+          src="https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js"
+          strategy="beforeInteractive"
+        ></Script>
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-LNH0V18K4F');
+        `}
+        </Script>
+      </body>
+    </Html>
+  );
+}
