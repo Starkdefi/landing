3e9bda521c92bf798823e664d336cc1e080261f9
Iddriss Raaj <idd.iraaj@gmail.com>
2023-02-13 17:20:23 +0000

    update team

--- components/sections/team/index.js
+++ components/sections/team/index.js
@@ -2,57 +2,53 @@ import { SimpleLink, Title } from "../../utils";
 import styles from "./team.module.css";
 import { Fade } from "react-awesome-reveal";
 import { TeamCard } from "../../team-card";
-import masamoon from "../../../assets/team/Masamoon.jpeg";
 import patrick from "../../../assets/team/Patrick.jpeg";
 import godsway from "../../../assets/team/Godsway.jpeg";
 import neslisah from "../../../assets/team/neslisah.jpeg";
 import iddriss from "../../../assets/team/Iddriss.jpeg";
-import brabbit from "../../../assets/team/Brabbit.jpeg";
+import zak from "../../../assets/team/Zak.jpeg";
 
 const team = [
   {
-    image: masamoon,
-    name: "Masamoon",
-    className: "xl:[grid-area:1/1/7/7] [grid-area:1/1/6/6] z-[0]",
-  },
-  {
     image: patrick,
     name: "Patrick Hemming",
-    className:
-      "xl:[grid-area:1/8/7/14] [grid-area:1/8/6/14] ml-[1vmax] z-[1] lg:ml-8 lg:-mr-4",
-  },
-  {
-    image: godsway,
-    name: "Godsway Annih",
-    className: "xl:[grid-area:9/1/14/6] [grid-area:9/1/13/5] z-[1]",
-  },
-  {
-    image: neslisah,
-    name: "M.Neslişah Suiçmez",
-    className:
-      "xl:[grid-area:6/5/12/10] xl:m-0 [grid-area:5/5/10/10] -ml-4 mr-8 z-[1]",
+    className: "[grid-area:1/1/4/5] pl-5 -mr-4 z-[1]",
   },
   {
     image: iddriss,
     name: "Iddriss Raaj",
-    className:
-      "xl:[grid-area:7/11/11/15] xl:m-0 [grid-area:6/10/9/15] -ml-4 mr-4 mt-2 z-[0]",
+    className: "[grid-area:1/9/3/12] pt-4 -mb-4 -ml-4 z-[3]",
   },
   {
-    image: brabbit,
-    name: "B-Rabbit",
-    className:
-      "xl:[grid-area:11/8/14/12] xl:m-0 xl:mt-[1vmax] [grid-area:11/7/15/11] -mt-4 -mr-4 ml-4 z-[0]",
+    image: godsway,
+    name: "Godsway Annih",
+    className: "[grid-area:5/8/8/12] z-[5] mr-2",
   },
+  {
+    image: neslisah,
+    name: "M.Neslişah Suiçmez",
+    className: "[grid-area:5/1/9/5] pl-2 z-[4]",
+  },
+  {
+    image: zak,
+    name: "Ismael Zak",
+    className: "[grid-area:3/5/7/9] -ml-2 z-[2]",
+  },
+  
 ];
 export function Team() {
   return (
     <section
       className={`${styles.container} 3xl:max-container flex items-center justify-between md:flex-row flex-col  xl:py-40 lg:py-32 py-24 md:flex-nowrap  flex-wrap`}
     >
-      <div className="md:w-1/2 w-full xl:h-[598px] lg:h-[540px] h-[400px] grid xl:grid-cols-[repeat(14,minmax(0,1fr))] xl:grid-rows-[repeat(15,minmax(0,1fr))] grid-cols-[repeat(12,minmax(0,1fr))] grid-rows-[repeat(14,minmax(0,1fr))] mb-8">
-        {team.map(({ image, name, className }, key) => (
-          <TeamCard key={key} image={image} name={name} className={className} />
+      <div className="md:w-1/2 w-full grid grid-cols-11 grid-rows-[repeat(7,minmax(0,1fr))] mb-8">
+        {team.map(({ image, name, className }) => (
+          <TeamCard
+            key={name}
+            image={image}
+            name={name}
+            className={className}
+          />
         ))}
       </div>
 
